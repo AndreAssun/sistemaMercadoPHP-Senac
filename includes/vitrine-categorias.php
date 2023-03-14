fdbd6876615e51b0bcc356807b4c65eba0514ef0
Andre Assunção <104846613+AndreAssun@users.noreply.github.com>
2023-03-13 21:57:17 -0300

    att de código na vitrine, página html gerada.
    Página HTML sendo gerada automaticamente.
    Mudança no banco de dados da aula 08, criação de tbl_lista_produtos.

--- includes/vitrine-categorias.php
+++ includes/vitrine-categorias.php
@@ -1,4 +1,7 @@
+<?php include_once("../config/conexao.php");?>
+
 <html>
+    
 <style>
     img{
         max-width:80px;
@@ -13,9 +16,17 @@ $query = "SELECT nome, slug, img FROM tbl_categorias";
 $resultado = mysqli_query($conexao, $query);
 $dados_categorias = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 
+
 foreach ($dados_categorias as $categoria) {
-    echo"<h4>".$categoria["nome"]. "</h4>";
-    echo"<img src='".$categoria["img"]. "'>";
+$txt = "<h2>Seja bem vindo<?h2><br>".$categoria["nome"];
+$lista_produtos = ""
+$myFile = fopen($categoria["slug"] . ".html" , "w");
+fwrite ($myFile, $txt);
+fclose ($myFile);
+
+
+echo"<h4>". $categoria["nome"]. "</h4>";
+echo"<img src='". $categoria["img"]. "'>";
 
 }
 
